d5c5037e10130d82ddc3c4dd25b7b304babe53fa
woshihuo12 <253268499@qq.com>
2016-03-29 09:33:29 +0800

    修改代码

--- UnityHello/Assets/Lua/common/simple_state_machine.lua
+++ UnityHello/Assets/Lua/common/simple_state_machine.lua
@@ -28,77 +28,77 @@ simple_state_machine.ASYNC = "ASYNC"
 function simple_state_machine:init()
 end
 
-function simple_state_machine:SetupState(cfg)
-    assert(type(cfg) == "table", "simple_state_machine:SetupState() - invalid config")
+function simple_state_machine:setup_state(cfg)
+    assert(type(cfg) == "table", "simple_state_machine:setup_state() - invalid config")
 
     -- cfg.initial allow for a simple string,
     -- or an table with { state = "foo", event = "setup", defer = true|false }
     if type(cfg.initial) == "string" then
-        self.initial_ = { state = cfg.initial }
+        self._initial = { state = cfg.initial }
     else
-        self.initial_ = cfg.initial
+        self._initial = cfg.initial
     end
 
-    self.terminal_ = cfg.terminal or cfg.final
-    self.events_ = cfg.events or { }
-    self.callbacks_ = cfg.callbacks or { }
-    self.map_ = { }
-    self.current_ = "none"
-    self.inTransition_ = false
+    self._terminal = cfg.terminal or cfg.final
+    self._events = cfg.events or { }
+    self._callbacks = cfg.callbacks or { }
+    self._map = { }
+    self._current = "none"
+    self._in_transition = false
 
-    if self.initial_ then
-        self.initial_.event = self.initial_.event or "startup"
-        self:addEvent_( { name = self.initial_.event, from = "none", to = self.initial_.state })
+    if self._initial then
+        self._initial.event = self._initial.event or "startup"
+        self:_add_event( { name = self._initial.event, from = "none", to = self._initial.state })
     end
 
-    for _, event in ipairs(self.events_) do
-        self:addEvent_(event)
+    for _, event in ipairs(self._events) do
+        self:_add_event(event)
     end
 
-    if self.initial_ and not self.initial_.defer then
-        self:doEvent(self.initial_.event)
+    if self._initial and not self._initial.defer then
+        self:do_event(self._initial.event)
     end
 
-    return self.target_
+    return self._target
 end
 
-function simple_state_machine:IsReady()
-    return self.current_ ~= "none"
+function simple_state_machine:is_ready()
+    return self._current ~= "none"
 end
 
-function simple_state_machine:GetState()
-    return self.current_
+function simple_state_machine:get_state()
+    return self._current
 end
 
-function simple_state_machine:IsState(state)
+function simple_state_machine:is_state(state)
     if type(state) == "table" then
         for _, s in ipairs(state) do
-            if s == self.current_ then
+            if s == self._current then
                 return true
             end
         end
         return false
     else
-        return self.current_ == state
+        return self._current == state
     end
 end
 
-function simple_state_machine:CanDoEvent(eventName)
-    return not self.inTransition_ and
-    (self.map_[eventName][self.current_] ~= nil or self.map_[eventName][simple_state_machine.WILDCARD] ~= nil)
+function simple_state_machine:can_do_event(event_name)
+    return not self._in_transition and
+    (self._map[event_name][self._current] ~= nil or self._map[event_name][simple_state_machine.WILDCARD] ~= nil)
 end
 
-function simple_state_machine:CannotDoEvent(eventName)
-    return not self:CanDoEvent(eventName)
+function simple_state_machine:cannot_do_event(event_name)
+    return not self:can_do_event(event_name)
 end
 
-function simple_state_machine:IsFinishedState()
-    return self:IsState(self.terminal_)
+function simple_state_machine:is_finished_state()
+    return self:is_state(self._terminal)
 end
 
-function simple_state_machine:DoEventForce(name, ...)
-    local from = self.current_
-    local map = self.map_[name]
+function simple_state_machine:do_event_force(name, ...)
+    local from = self._current
+    local map = self._map[name]
     local to =(map[from] or map[simple_state_machine.WILDCARD]) or from
     local args = { ...}
 
@@ -109,29 +109,29 @@ function simple_state_machine:DoEventForce(name, ...)
         args = args
     }
 
-    if self.inTransition_ then
-        self.inTransition_ = false
+    if self._in_transition then
+        self._in_transition = false
     end
 
-    self:beforeEvent_(event)
+    self:_before_event(event)
 
     if from == to then
-        self:afterEvent_(event)
+        self:_after_event(event)
         return simple_state_machine.NOTRANSITION
     end
 
-    self.current_ = to
-    self:enterState_(event)
-    self:changeState_(event)
-    self:afterEvent_(event)
+    self._current = to
+    self:_enter_state(event)
+    self:_change_state(event)
+    self:_after_event(event)
     return simple_state_machine.SUCCEEDED
 end
 
-function simple_state_machine:DoEvent(name, ...)
-    assert(self.map_[name] ~= nil, string.format("simple_state_machine:DoEvent() - invalid event %s", tostring(name)))
+function simple_state_machine:do_event(name, ...)
+    assert(self._map[name] ~= nil, string.format("simple_state_machine:DoEvent() - invalid event %s", tostring(name)))
 
-    local from = self.current_
-    local map = self.map_[name]
+    local from = self._current
+    local map = self._map[name]
     local to =(map[from] or map[simple_state_machine.WILDCARD]) or from
     local args = { ...}
 
@@ -142,49 +142,49 @@ function simple_state_machine:DoEvent(name, ...)
         args = args,
     }
 
-    if self.inTransition_ then
-        self:onError_(event, simple_state_machine.PENDING_TRANSITION_ERROR,
+    if self._in_transition then
+        self:_on_error(event, simple_state_machine.PENDING_TRANSITION_ERROR,
         "event " .. name .. " inappropriate because previous transition did not complete")
         return simple_state_machine.FAILURE
     end
 
-    if self:CannotDoEvent(name) then
-        self:onError_(event, simple_state_machine.INVALID_TRANSITION_ERROR,
-        "event " .. name .. " inappropriate in current state " .. self.current_)
+    if self:cannot_do_event(name) then
+        self:_on_error(event, simple_state_machine.INVALID_TRANSITION_ERROR,
+        "event " .. name .. " inappropriate in current state " .. self._current)
         return simple_state_machine.FAILURE
     end
 
-    if self:beforeEvent_(event) == false then
+    if self:_before_event(event) == false then
         return simple_state_machine.CANCELLED
     end
 
     if from == to then
-        self:afterEvent_(event)
+        self:_after_event(event)
         return simple_state_machine.NOTRANSITION
     end
 
     event.transition = function()
-        self.inTransition_ = false
-        self.current_ = to
+        self._in_transition = false
+        self._current = to
         -- this method should only ever be called once
-        self:enterState_(event)
-        self:changeState_(event)
-        self:afterEvent_(event)
+        self:_enter_state(event)
+        self:_change_state(event)
+        self:_after_event(event)
         return simple_state_machine.SUCCEEDED
     end
 
     event.cancel = function()
         -- provide a way for caller to cancel async transition if desired
         event.transition = nil
-        self:afterEvent_(event)
+        self:_after_event(event)
     end
 
-    self.inTransition_ = true
-    local leave = self:leaveState_(event)
+    self._in_transition = true
+    local leave = self:_leave_state(event)
     if leave == false then
         event.transition = nil
         event.cancel = nil
-        self.inTransition_ = false
+        self._in_transition = false
         return simple_state_machine.CANCELLED
     elseif string.upper(tostring(leave)) == simple_state_machine.ASYNC then
         return simple_state_machine.PENDING
@@ -194,12 +194,12 @@ function simple_state_machine:DoEvent(name, ...)
         if event.transition then
             return event.transition()
         else
-            self.inTransition_ = false
+            self._in_transition = false
         end
     end
 end
 
-function simple_state_machine:addEvent_(event)
+function simple_state_machine:_add_event(event)
     local from = { }
     if type(event.from) == "table" then
         for _, name in ipairs(event.from) do
@@ -212,69 +212,69 @@ function simple_state_machine:addEvent_(event)
         from[simple_state_machine.WILDCARD] = true
     end
 
-    self.map_[event.name] = self.map_[event.name] or { }
-    local map = self.map_[event.name]
+    self._map[event.name] = self._map[event.name] or { }
+    local map = self._map[event.name]
     for fromName, _ in pairs(from) do
         map[fromName] = event.to or fromName
     end
 end
 
-local function doCallback_(callback, event)
+local function _do_callback(callback, event)
     if callback then
         return callback(event)
     end
 end
 
-function simple_state_machine:beforeAnyEvent_(event)
-    return doCallback_(self.callbacks_["onbeforeevent"], event)
+function simple_state_machine:_before_any_event(event)
+    return _do_callback(self._callbacks["onbeforeevent"], event)
 end
 
-function simple_state_machine:afterAnyEvent_(event)
-    return doCallback_(self.callbacks_["onafterevent"], event)
+function simple_state_machine:_after_any_event(event)
+    return _do_callback(self._callbacks["onafterevent"], event)
 end
 
-function simple_state_machine:leaveAnyState_(event)
-    return doCallback_(self.callbacks_["onleavestate"], event)
+function simple_state_machine:_leave_any_state(event)
+    return _do_callback(self._callbacks["onleavestate"], event)
 end
 
-function simple_state_machine:enterAnyState_(event)
-    return doCallback_(self.callbacks_["onenterstate"] or self.callbacks_["onstate"], event)
+function simple_state_machine:_enter_any_state(event)
+    return _do_callback(self._callbacks["onenterstate"] or self._callbacks["onstate"], event)
 end
 
-function simple_state_machine:changeState_(event)
-    return doCallback_(self.callbacks_["onchangestate"], event)
+function simple_state_machine:_change_state(event)
+    return _do_callback(self._callbacks["onchangestate"], event)
 end
 
-function simple_state_machine:beforeThisEvent_(event)
-    return doCallback_(self.callbacks_["onbefore" .. event.name], event)
+function simple_state_machine:_before_this_event(event)
+    return _do_callback(self._callbacks["onbefore" .. event.name], event)
 end
 
-function simple_state_machine:afterThisEvent_(event)
-    return doCallback_(self.callbacks_["onafter" .. event.name] or self.callbacks_["on" .. event.name], event)
+function simple_state_machine:_after_this_event(event)
+    return _do_callback(self._callbacks["onafter" .. event.name] or self._callbacks["on" .. event.name], event)
 end
 
-function simple_state_machine:leaveThisState_(event)
-    return doCallback_(self.callbacks_["onleave" .. event.from], event)
+function simple_state_machine:_leave_this_state(event)
+    return _do_callback(self._callbacks["onleave" .. event.from], event)
 end
 
-function simple_state_machine:enterThisState_(event)
-    return doCallback_(self.callbacks_["onenter" .. event.to] or self.callbacks_["on" .. event.to], event)
+function simple_state_machine:_enter_this_state(event)
+    return _do_callback(self._callbacks["onenter" .. event.to] or self._callbacks["on" .. event.to], event)
 end
 
-function simple_state_machine:beforeEvent_(event)
-    if self:beforeThisEvent_(event) == false or self:beforeAnyEvent_(event) == false then
+function simple_state_machine:_before_event(event)
+    if self:_before_this_event(event) == false or self:_before_any_event(event) == false then
         return false
     end
 end
 
-function simple_state_machine:afterEvent_(event)
-    self:afterThisEvent_(event)
-    self:afterAnyEvent_(event)
+function simple_state_machine:_after_event(event)
+    self:_after_this_event(event)
+    self:_after_any_event(event)
 end
 
-function simple_state_machine:leaveState_(event, transition)
-    local specific = self:leaveThisState_(event, transition)
-    local general = self:leaveAnyState_(event, transition)
+function simple_state_machine:_leave_state(event, transition)
+    local specific = self:_leave_this_state(event, transition)
+    local general = self:_leave_any_state(event, transition)
     if specific == false or general == false then
         return false
     elseif string.upper(tostring(specific)) == simple_state_machine.ASYNC
@@ -283,13 +283,13 @@ function simple_state_machine:leaveState_(event, transition)
     end
 end
 
-function simple_state_machine:enterState_(event)
-    self:enterThisState_(event)
-    self:enterAnyState_(event)
+function simple_state_machine:_enter_state(event)
+    self:_enter_this_state(event)
+    self:_enter_any_state(event)
 end
 
-function simple_state_machine:onError_(event, error, message)
-    print("%s [simple_state_machine] ERROR: error %s, event %s, from %s to %s", tostring(self.target_), tostring(error), event.name, event.from, event.to)
+function simple_state_machine:_on_error(event, error, message)
+    print("%s [simple_state_machine] ERROR: error %s, event %s, from %s to %s", tostring(self._target), tostring(error), event.name, event.from, event.to)
     print(message)
 end
 
