3801c65e25a45eac5cfdf5a9966ec31f17cced1a
woshihuo12 <253268499@qq.com>
2016-04-05 12:08:34 +0800

    添加 textoutline 小示例

---
+++ UnityHello/Assets/Game/Scripts/UI/UITextOutline.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UITextOutline : Shadow
+{
+    [Range(2, 16)]
+    [SerializeField]
+    private int mDivideAmoumt = 8;
+
+    protected UITextOutline()
+    {
+
+    }
+
+    public override void ModifyMesh(VertexHelper vh)
+    {
+        if (!IsActive())
+        {
+            return;
+        }
+
+        List<UIVertex> verts = new List<UIVertex>();
+        vh.GetUIVertexStream(verts);
+
+        if (verts == null || verts.Count == 0)
+        {
+            return;
+        }
+
+        int start;
+        int end = 0;
+
+        for (float i = 0; i <= Mathf.PI * 2; i += Mathf.PI / (float)mDivideAmoumt)
+        {
+            start = end;
+            end = verts.Count;
+            ApplyShadow(verts, effectColor, start, end, effectDistance.x * Mathf.Cos(i), effectDistance.y * Mathf.Sin(i));
+        }
+
+        vh.Clear();
+        vh.AddUIVertexTriangleStream(verts);
+    }
+}
