11721b311c44634f6f63272c1c07a80921c5269a
woshihuo12 <253268499@qq.com>
2016-04-14 12:18:21 +0800

    更新类图

--- UnityHello/Assets/Game/Scripts/DTPathFind/DetourStatus.cs
+++ UnityHello/Assets/Game/Scripts/DTPathFind/DetourStatus.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using dtStatus = System.UInt32;
 
 namespace DTPathFind
 {
@@ -13,6 +12,29 @@ namespace DTPathFind
         public const uint DT_WRONG_MAGIC = 1 << 0;
         public const uint DT_WRONG_VERSION = 1 << 1;
         public const uint DT_OUT_OF_MEMORY = 1 << 2;
+        public const uint DT_INVALID_PARAM = 1 << 3;
+        public const uint DT_BUFFER_TOO_SMALL = 1 << 4;
+        public const uint DT_OUT_OF_NODES = 1 << 5;
+        public const uint DT_PARTIAL_RESULT = 1 << 6;
 
+        public static bool dtStatusSucceed(dtStatus status)
+        {
+            return (status & DT_SUCCESS) != 0;
+        }
+
+        public static bool dtStatusFailed(dtStatus status)
+        {
+            return (status & DT_FAILURE) != 0;
+        }
+
+        public static bool dtStatusInProgress(dtStatus status)
+        {
+            return (status & DT_IN_PROGRESS) != 0;
+        }
+
+        public static bool dtStatusDetail(dtStatus status, uint detail)
+        {
+            return (status & detail) != 0;
+        }
     }
 }
